1a6f57a76f6a0fd6084e27c43bfbee1359c777d0
Douglas Mendes <douglasleomendes@hotmail.com>
2024-05-01 12:41:24 +0100

    chore: add some messages error for login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,39 +1,49 @@
 import React, { useState } from 'react';
-import { auth, signInWithPopup,signInWithEmailAndPassword, GoogleAuthProvider } from '../firebase/firebase';
+import { auth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from '../firebase/firebase';
 import { useNavigate } from 'react-router-dom';
 import { FaGoogle } from 'react-icons/fa';
-
-
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   const handleLoginEmailAndPassword = async () => {
+    if (!email || !password) {
+      setError('Email and password are required.');
+      return;
+    }
     try {
       await signInWithEmailAndPassword(auth, email, password);
       console.log('Login successful!');
-      navigate('/home')
+      navigate('/home');
     } catch (error) {
       console.error('Error logging in:', error);
+      if (error.code === 'auth/user-not-found'){
+        setError('User not registered. Please sign up.');
+      }else if( error.code === 'auth/wrong-password' || error.code === 'auth/invalid-email' || error.code === 'auth/invalid-credential'){
+        setError('Error logging in. Please check your email and password.');
+      }else if ( error.code === 'auth/too-many-requests'){
+        setError('This user has been temporarily disabled due to too many failed login attempts. Please try again later or reset your password.');
+      }
     }
   };
   
   const handleGoogleLogin = async () => {
-    try{
+    try {
       const provider = new GoogleAuthProvider();
       await signInWithPopup(auth, provider);
       console.log('Login successful!');
-      navigate('/home')
+      navigate('/home');
     } catch (error) {
       console.error('Error logging in:', error);
+      setError('An error occurred while logging in with Google. Please try again later.');
     }
   };
 
-
   const handleSignUpRedirect = () => {
-    navigate('/signup')
+    navigate('/signup');
   }
 
   return (
@@ -54,6 +64,7 @@ const Login = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        {error && <p className="text-red-500 mb-4">{error}</p>}
         <button
           className="w-full px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
           onClick={handleLoginEmailAndPassword}
