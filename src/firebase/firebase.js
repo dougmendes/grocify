6ab85f9354d1a25071c9fdd3bba82b86484e924a
Douglas Mendes <douglasleomendes@hotmail.com>
2024-05-02 12:22:08 +0100

    remove: cypress

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,6 +2,8 @@ import {initializeApp} from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 
 
+require('dotenv').config();
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
